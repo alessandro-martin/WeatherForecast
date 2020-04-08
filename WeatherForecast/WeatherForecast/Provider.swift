500f7dd3c7468879714bfe462ee8dd1559c8d8db
Alessandro Martin <martin.acg@gmail.com>
2020-04-08 16:52:00 +0100

    Improved URL creation

--- WeatherForecast/WeatherForecast/Provider.swift
+++ WeatherForecast/WeatherForecast/Provider.swift
@@ -11,12 +11,24 @@ import Foundation
 
 enum Provider {
     static func weather(city: String) -> AnyPublisher<Response, Never> {
-        let url = URL(string: "https://samples.openweathermap.org/data/2.5/forecast?q=London,us&appid=b6907d289e10d714a6e88b30761fae22")!
-        return URLSession.shared
-            .dataTaskPublisher(for: url)
+        URLSession.shared
+            .dataTaskPublisher(for: Provider.url(city: city))
             .map(\.data)
             .decode(type: Response.self, decoder: JSONDecoder())
             .replaceError(with: .empty)
             .eraseToAnyPublisher()
+    }
+    
+    private static func url(city: String) -> URL {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "samples.openweathermap.org"
+        components.path = "/data/2.5/forecast"
+        components.queryItems = [
+            .init(name: "q", value: city + ",us"),
+            .init(name: "appid", value: "b6907d289e10d714a6e88b30761fae22")
+        ]
+        
+        return components.url!
     }
 }
