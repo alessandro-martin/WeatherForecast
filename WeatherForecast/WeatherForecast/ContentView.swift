b27f25510734760cf0e3694f7c4a10a8c35eab3f
Alessandro Martin <martin.acg@gmail.com>
2020-04-08 15:27:05 +0100

    Project Creation

---
+++ WeatherForecast/WeatherForecast/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherForecast
+//
+//  Created by Alessandro Martin on 08/04/2020.
+//  Copyright © 2020 Alessandro Martin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
